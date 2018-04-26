5bd7778f358fb8f6278b4c4127001df3274aeaa4
Lucia Saura <nayade_2lu@hotmail.com>
2018-04-26 20:45:08 +0100

    numpy import

--- irisdataset-analysis.py
+++ irisdataset-analysis.py
@@ -1,3 +1,6 @@
 #Lucia Saura 26/04/2018
 #Iris Data set analysis https://archive.ics.uci.edu/ml/datasets/iris
 #Project Programming and Scripting module
+
+#Calculate the mean of each Setosa parameter
+import numpy
