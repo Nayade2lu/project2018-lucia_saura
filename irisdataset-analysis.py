17867227d50a576d4a4b9af53bdf72f68f06825a
Lucia Saura <nayade_2lu@hotmail.com>
2018-04-28 10:28:19 +0100

    commented

--- irisdataset-analysis.py
+++ irisdataset-analysis.py
@@ -3,7 +3,13 @@
 #Project Programming and Scripting module
 
 #Calculate the mean of each Setosa parameter
+#Improting the numpy library
 import numpy
+#Read setosa data file into array
 data = numpy.genfromtxt('iris-data-set.csv', delimiter=',')
+#selecting the first column of the data
 sepallenghtsetosa = data[:,0]
+#calculating the mean of the first column
 meansepallengthsetosa = numpy.mean(data[:,0])
+#Code tested working in iPhyton
+
