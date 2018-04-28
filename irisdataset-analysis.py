a2c5739711176fd6fa0a311b393951ea1f8c2efa
Lucia Saura <nayade_2lu@hotmail.com>
2018-04-28 10:51:04 +0100

    setosa sepal width

--- irisdataset-analysis.py
+++ irisdataset-analysis.py
@@ -3,6 +3,8 @@
 #Project Programming and Scripting module
 
 #Calculate the mean of each Setosa parameter
+
+#Calculate Setosa sepal length mean
 #Improting the numpy library
 import numpy
 #Read setosa data file into array
@@ -12,3 +14,18 @@ sepallenghtsetosa = data[:,0]
 #calculating the mean of the first column
 meansepallengthsetosa = numpy.mean(data[:,0])
 #Code tested working in iPhyton
+
+#Calcualate Setosa sepal width mean
+#Improting the numpy library
+import numpy
+#Read setosa data file into array
+data = numpy.genfromtxt('iris-data-set.csv', delimiter=',')
+#selecting the first column of the data
+sepalwidthtsetosa = data[:,1]
+#calculating the mean of the first column
+meansepalwidthsetosa = numpy.mean(data[:,1])
+#Code tested working in iPhyton
+
+
+
+
