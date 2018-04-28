ec5e102332d8750c36d7c77a4dfd018f7c74bb3c
Lucia Saura <nayade_2lu@hotmail.com>
2018-04-28 13:59:12 +0100

    printing the values + Bug fixing (need to import numpy each time)

--- irisdataset-analysis.py
+++ irisdataset-analysis.py
@@ -8,49 +8,22 @@
 #Improting the numpy library
 import numpy
 #Read setosa data file into array
-data = numpy.genfromtxt('iris-data-set.csv', delimiter=',')
+data = numpy.genfromtxt('iris-data-set-setosa.csv', delimiter=',')
 #selecting the first column of the data
 sepallenghtsetosa = data[:,0]
 #calculating the mean of the first column
 meansepallengthsetosa = numpy.mean(data[:,0])
 #Code tested working in iPhyton
+print(meansepallengthsetosa)
 
 #Calcualate Setosa sepal width mean
 #Improting the numpy library
 import numpy
 #Read setosa data file into array
-data = numpy.genfromtxt('iris-data-set.csv', delimiter=',')
+data = numpy.genfromtxt('iris-data-set-setosa.csv', delimiter=',')
 #selecting the second column of the data
 sepalwidthtsetosa = data[:,1]
 #calculating the mean of the second column
 meansepalwidthsetosa = numpy.mean(data[:,1])
-#Code tested working in iPhyton
 
-#Calcualate Setosa petal length mean
-#Improting the numpy library
-import numpy
-#Read setosa data file into array
-data = numpy.genfromtxt('iris-data-set.csv', delimiter=',')
-#selecting the third column of the data
-petallengthtsetosa = data[:,2]
-#calculating the mean of the third column
-meanpetallengthsetosa = numpy.mean(data[:,2])
-#Code tested working in iPhyton
-
-#Calcualate Setosa petal width mean
-#Improting the numpy library
-import numpy
-#Read setosa data file into array
-data = numpy.genfromtxt('iris-data-set.csv', delimiter=',')
-#selecting the fourth column of the data
-petalwidthtsetosa = data[:,3]
-#calculating the mean of the fourth column
-meanpetalwidthsetosa = numpy.mean(data[:,3])
-#Code tested working in iPhyton
-
-
-
-
-
-
-
+print(meansepalwidthsetosa)
