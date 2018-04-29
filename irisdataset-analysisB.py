7d35634b2ec5c104dc135afe4bb713aea7e7a38d
Lucia Saura <nayade_2lu@hotmail.com>
2018-04-29 11:58:36 +0100

    petal length hitogram (setosa, virginica, versicolor)

--- irisdataset-analysisB.py
+++ irisdataset-analysisB.py
@@ -1,11 +1,12 @@
 #Lucia Saura 28/04/2018
 # approach matplotlib histogram
 
+#Sepal length-------
 import numpy
 #Read setosa data file into array
 data = numpy.genfromtxt('iris-data-set-setosa.csv', delimiter=',')
 # show data from sepal length setosa
-firstcol = data[:,3]
+firstcol = data[:,0]
 #import the library matplot
 import matplotlib.pyplot as pl
 #create a histogram of sepal lenght setosa
@@ -17,7 +18,7 @@ pl.show()
 #Read virginica data file into array
 data = numpy.genfromtxt('iris-data-set-virginica.csv', delimiter=',')
 # show data from sepal length virginica
-firstcol = data[:,3]
+firstcol = data[:,0]
 
 import matplotlib.pyplot as pl
 pl.hist(firstcol)
@@ -27,10 +28,78 @@ pl.show()
 #Read versicolor data file into array
 data = numpy.genfromtxt('iris-data-set-versicolor.csv', delimiter=',')
 # show data from sepal length versicolor
-firstcol = data[:,2]
+firstcol = data[:,0]
 
 import matplotlib.pyplot as pl
 pl.hist(firstcol)
+pl.show()
+
+
+
+#Sepal width------------------
+
+#Read setosa data file into array
+data = numpy.genfromtxt('iris-data-set-setosa.csv', delimiter=',')
+# show data from sepal width setosa
+seccol = data[:,1]
+#import the library matplot
+import matplotlib.pyplot as pl
+#create a histogram of sepal width setosa
+pl.hist(seccol)
+#showing the histogram
+pl.show()
+
+
+#Read virginica data file into array
+data = numpy.genfromtxt('iris-data-set-virginica.csv', delimiter=',')
+# show data from sepal width virginica
+seccol = data[:,1]
+
+import matplotlib.pyplot as pl
+pl.hist(seccol)
+pl.show()
+
+
+#Read versicolor data file into array
+data = numpy.genfromtxt('iris-data-set-versicolor.csv', delimiter=',')
+# show data from sepal width versicolor
+seccol = data[:,1]
+
+import matplotlib.pyplot as pl
+pl.hist(seccol)
+pl.show()
+
+#Petal length------------------
+
+#Read setosa data file into array
+data = numpy.genfromtxt('iris-data-set-setosa.csv', delimiter=',')
+# show data from petal length setosa
+thirdcol = data[:,2]
+#import the library matplot
+import matplotlib.pyplot as pl
+#create a histogram of petal lenght setosa
+pl.hist(thirdcol)
+#showing the histogram
+pl.show()
+
+
+#Read virginica data file into array
+data = numpy.genfromtxt('iris-data-set-virginica.csv', delimiter=',')
+# show data from petal length virginica
+thirdcol = data[:,2]
+
+import matplotlib.pyplot as pl
+pl.hist(thirdcol)
+pl.show()
+
+
+#Read versicolor data file into array
+data = numpy.genfromtxt('iris-data-set-versicolor.csv', delimiter=',')
+# show data from sepal length versicolor
+thirdcol = data[:,2]
+
+import matplotlib.pyplot as pl
+pl.hist(thirdcol)
 pl.show()
 
 
