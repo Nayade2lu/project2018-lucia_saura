d02bd5005aabaff656baf0e821181c16236eb707
Lucia Saura <nayade_2lu@hotmail.com>
2018-04-29 13:15:03 +0100

    Additional comments

--- irisdataset-analysisB.py
+++ irisdataset-analysisB.py
@@ -1,7 +1,8 @@
 #Lucia Saura 28/04/2018
 # approach matplotlib histogram
+# Reference GMIT module programming and scripting: Topic 8 matplotlib
 
-#Sepal length-------
+#Setosa--------------------------
 import numpy
 #Read setosa data file into array
 data = numpy.genfromtxt('iris-data-set-setosa.csv', delimiter=',')
@@ -31,5 +32,65 @@ foucolm = data[:,3]
 pl.hist(foucolm)
 #showing the histogram
 pl.show()
+
+
+
+#Virginica---------------------
+#Virginica data file into array
+data = numpy.genfromtxt('iris-data-set-virginica.csv', delimiter=',')
+# show data from sepal length virginica
+firstcol = data[:,0]
+import matplotlib.pyplot as pl
+pl.hist(firstcol)
+pl.show()
+
+seccol = data[:,1]
+#create a histogram of sepal width virginica
+pl.hist(seccol)
+#showing the histogram
+pl.show()
+
+thirdcol = data[:,2]
+#create a histogram of petal lenght virginica
+pl.hist(thirdcol)
+#showing the histogram
+pl.show()
+
+foucolm = data[:,3]
+#create a histogram of petal width virginica
+pl.hist(foucolm)
+#showing the histogram
+pl.show()
+
+
+
+#Versicolor--------------------------
+#Read versicolor data file into array
+data = numpy.genfromtxt('iris-data-set-versicolor.csv', delimiter=',')
+# show data from sepal length versicolor
+firstcol = data[:,0]
+
+import matplotlib.pyplot as pl
+pl.hist(firstcol)
+pl.show()
+
+seccol = data[:,1]
+#create a histogram of sepal width versicolor
+pl.hist(seccol)
+#showing the histogram
+pl.show()
+
+thirdcol = data[:,2]
+#create a histogram of petal lenght versicolor
+pl.hist(thirdcol)
+#showing the histogram
+pl.show()
+
+foucolm = data[:,3]
+#create a histogram of petal width versicolor
+pl.hist(foucolm)
+#showing the histogram
+pl.show()
+
 
 
