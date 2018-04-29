547793de2702cdfea0847a7b91f3a2db140f813a
Lucia Saura <nayade_2lu@hotmail.com>
2018-04-29 01:18:27 +0100

    setosa virginica and vericolor sepal length

--- irisdataset-analysisB.py
+++ irisdataset-analysisB.py
@@ -4,17 +4,19 @@
 import numpy
 #Read setosa data file into array
 data = numpy.genfromtxt('iris-data-set-setosa.csv', delimiter=',')
-
+# show data from sepal length setosa
 firstcol = data[:,0]
-
+#import the library matplot
 import matplotlib.pyplot as pl
+#create a histogram of sepal lenght setosa
 pl.hist(firstcol)
+#showing the histogram
 pl.show()
 
 
 #Read virginica data file into array
 data = numpy.genfromtxt('iris-data-set-virginica.csv', delimiter=',')
-
+# show data from sepal length virginica
 firstcol = data[:,0]
 
 import matplotlib.pyplot as pl
@@ -24,10 +26,12 @@ pl.show()
 
 #Read versicolor data file into array
 data = numpy.genfromtxt('iris-data-set-versicolor.csv', delimiter=',')
-
+# show data from sepal length versicolor
 firstcol = data[:,0]
 
 import matplotlib.pyplot as pl
 pl.hist(firstcol)
 pl.show()
+
+
 
